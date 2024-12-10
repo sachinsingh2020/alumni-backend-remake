190a451d6a495f365aac3e39e3e648c44652503b
sachin singh <sachinthegreat1001@gmail.com>
2024-12-10 22:48:31 +0530

    remove images

--- config/database.js
+++ config/database.js
@@ -5,3 +5,5 @@ export const connectDB = async () => {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected with ${connection.host}`);
 };
+
+// sachin 
